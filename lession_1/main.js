b28ac91dc95616acedb841d0d4c96f7ef62a7bae
Trieu Luan <luanotse184297@fpt.edu.vn>
2025-05-14 13:50:04 +0700

    Test

---
+++ lession_1/main.js
@@ -0,0 +1,44 @@
+// function cong(a,b) {
+//     return a+b;
+// }
+// var result = cong(2,8);
+// console.log(result);
+// var myString = 'javascrip';
+// var con = 9;
+// console.log(myString.search('JS'));
+// console.log(myString.slice(0));
+// console.log(myString.replace(/JS/g,'JavaScrip'));
+// console.log(myString.toUpperCase());
+// console.log(myString.toLocaleLowerCase());
+// console.log(myString.trim());
+// console.log()
+// console.log( typeof myString[5]);
+// function isNumber(value){
+// if( typeof value==='number') return true;
+// else return false;
+
+// }
+
+
+
+
+// // Expected results:
+// console.log(isNumber(999)); // true
+// console.log(isNumber('abc')); // false
+// console.log(isNumber('100')); // false
+var newArray = ['a',
+    'b',
+    'c',
+    12,
+    11
+];
+// console.log(Array.isArray(newArray));
+// console.log(newArray.length);
+// console.log(newArray.push('dart','light'));
+// console.log(newArray);
+// console.log(newArray.unshift())
+// console.log(newArray.splice(1,1,'d'))
+var newArray2 = [2,7,5,4];
+// console.log(newArray.concat(newArray2));
+console.log(newArray.slice(-1))
+console.log(newArray);
